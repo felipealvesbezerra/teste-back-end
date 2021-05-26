c8ebee9c413b785c14e6b79207b5e188ecc1112d
Leonardo Guedes <leoguedes.14@hotmail.com>
2021-05-26 17:07:15 -0300

    added first route

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'kashin.db.elephantsql.com',
+    username: 'tmohtbji',
+    password: 'aHT2v5ltFR86eRQw7157qqNc9fbqmUdq',
+    database: 'tmohtbji',
+    define: {
+      timestamps: true,
+      underscored: true,
+    },
+  };
