ab37868689b1ced41266f39940d444f352cd8455
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-05-27 14:01:46 -0300

    fix catch error

--- src/data/connection.js
+++ src/data/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
     try {
         await mongoose.connect("mongodb+srv://teddy:101010@teddy.l5l15.mongodb.net/Teddy?retryWrites=true&w=majority");
         console.log("MongoDB Connected: ");
-    } catch (err) {
-      console.log('MongoDB ERROR: ' + err)
+    } catch (error) {
+      console.log('MongoDB ERROR: ' + error)
     }
 })();
