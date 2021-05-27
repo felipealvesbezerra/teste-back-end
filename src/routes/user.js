ae1a2bec54c1a0aea6debc278386b0b1b3c73511
slowed <rootslowed@gmail.com>
2021-05-27 16:15:10 -0300

    middleware removed

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,31 +1,13 @@
 import express from "express";
-import * as controller from "../controllers/user.js";
-import User from '../models/User.js';
+import * as userController from "../controllers/user.js";
+import getUser from "../middlewares/getUser.js";
 
 const router = express.Router();
 
-router.get("/", controller.getUsers);
-router.get("/:id", getUser, controller.getUserId);
-router.post("/", controller.postUser);
-router.patch("/:id", getUser, controller.patchUser);
-router.delete("/:id", getUser, controller.deleteUser);
-
-async function getUser (req, res, next) {
-
-  let user;
-
-  try {
-    user = await User.findById(req.params.id);
-  } catch (err) {
-    return res.status(404).json({
-      message: 'cannot find subscriber',
-    });
-  }
-
-  res.user = user;
-
-  next();
-
-}
+router.get("/", userController.getUsers);
+router.get("/:id", getUser, userController.getUserId);
+router.post("/", userController.postUser);
+router.patch("/:id", getUser, userController.patchUser);
+router.delete("/:id", getUser, userController.deleteUser);
 
 export default router;
