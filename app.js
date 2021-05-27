375c4a1d1577aad2073cdeb2eaf2b28bef2ae061
slowed <rootslowed@gmail.com>
2021-05-27 16:14:32 -0300

    port and mongouri

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ import dotenv from "dotenv"; dotenv.config();
 // APP
 const app = express();
 app.use(express.json());
+const PORT = process.env.PORT;
+const MONGOURI = process.env.MONGOURI;
 
 // ROUTER
 import userRouter from "./src/routes/user.js";
 app.use("/user", userRouter);
-
-const PORT = process.env.PORT;
-const MONGOURI = process.env.MONGOURI;
 
 // MONGOOSE
 try {
