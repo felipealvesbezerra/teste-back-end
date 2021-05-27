05a36aaa1f73262a938e5e1166ce7a28c0d3e6f5
Ronaldo Vasconcelos <ronaldo.asconcellos@gmail.com>
2021-05-27 12:50:35 -0300

    finish repository

--- src/repository/CompanyRepository.js
+++ src/repository/CompanyRepository.js
@@ -10,10 +10,46 @@ module.exports = class CompanyModel {
     }
 
   }
-	
+
 	static async create(model) {
     try {
       return await db.create(model); 
+    } catch (error) {
+      return error;
+    }
+  }
+
+	static async findById(id){
+    try {
+      const company = await db.findById({ _id: id });
+      return company;
+    } catch (error) {
+      return error;
+    }
+  }
+
+	static async update(id, model) {
+    try {
+      const company =  db.findByIdAndUpdate(id, model);
+      return company;
+    } catch (error) {
+      return error;
+    }
+  }
+
+	static async delete(id) {
+    try {
+      const company = await db.findByIdAndDelete(id);
+      return company;
+    } catch (error) {
+      return error;
+    }
+  }
+
+	static async existsCnpj(cnpj){
+    try {
+      const cnpjexists = await db.find({ cnpj: cnpj });
+      return cnpjexists;
     } catch (error) {
       return error;
     }
